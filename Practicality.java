8381d28b4c27dfde764481c5448404437104bb29
Satish <satishsyadav18@gmail.com>
2024-08-02 23:32:57 +0530

    message

--- Practicality.java
+++ Practicality.java
@@ -1,5 +1,5 @@
 public class Practicality {
     public static void main(String[] args) {
-        System.out.println("Satish singh yadav");
+        System.out.println("Satish  18 yadav");
     }
 }
